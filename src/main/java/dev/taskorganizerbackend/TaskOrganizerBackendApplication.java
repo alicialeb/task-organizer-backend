f07c7a07f161980f422c2a19f6d5feab1f7d2d4f
alicialeb <37578791+alicialeb@users.noreply.github.com>
2021-10-19 21:48:44 +0200

    Initial commit

---
+++ src/main/java/dev/taskorganizerbackend/TaskOrganizerBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.taskorganizerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskOrganizerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskOrganizerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/taskorganizerbackend/TaskOrganizerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.taskorganizerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskOrganizerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
